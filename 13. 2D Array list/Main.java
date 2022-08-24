9f8a82ee8a0e998ee811dd9ca936643838d5b9e5
Lakshay <Lakshaykamat2048@gmail.com>
2022-08-24 11:02:31 +0530

    new folders

--- 13. 2D Array list/Main.java
+++ 13. 2D Array list/Main.java
@@ -4,22 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//combining all string array lists so that's why this is called 3d
 		ArrayList<ArrayList<String>> groceryList = new ArrayList();
 		
+		//simpe 1 D array list
 		ArrayList<String> bakeryList = new ArrayList();
 		bakeryList.add("pasta");
 		bakeryList.add("garlic bread");
 		bakeryList.add("donuts");
 		
+		//simpe 1 D array list
 		ArrayList<String> produceList = new ArrayList();
 		produceList.add("tomatoes");
 		produceList.add("zucchini");
 		produceList.add("peppers");
 		
+		//simpe 1 D array list
 		ArrayList<String> drinksList = new ArrayList();
 		drinksList.add("soda");
 		drinksList.add("coffee");
-		
+
+		//adding item to 3d array list
 		groceryList.add(bakeryList);
 		groceryList.add(produceList);
 		groceryList.add(drinksList);
