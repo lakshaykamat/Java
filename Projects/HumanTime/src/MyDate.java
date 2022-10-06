7298d603be6c44c62ecbcc59ed8139ada302d8bb
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2022-10-06 08:54:39 +0530

    comments added

--- Projects/HumanTime/src/MyDate.java
+++ Projects/HumanTime/src/MyDate.java
@@ -9,10 +9,13 @@ public class MyDate {
     long currentDateMilliSecond;
     int totalDaysSpent, totalWeeksSpent, totalMonthSpent, totalYearSpent;
     MyDate(String usrDate) throws ParseException {
-        SimpleDateFormat formatter = new SimpleDateFormat("d/M/yyy");
-       Date formatDate = formatter.parse(usrDate);
-       this.usrDateMilliSecond =  formatDate.getTime();
-       this.usrDate = formatDate;
+        SimpleDateFormat formatter = new SimpleDateFormat("d/M/yyy");//Formatter of String to Date
+       Date formatDate = formatter.parse(usrDate);//Giving String to formatter
+       this.usrDateMilliSecond =  formatDate.getTime();//converting usr time into milliseconds
+       this.usrDate = formatDate;//storing usr given time
+
+        currentDatesAndMilliSecond();//Current Date and Millisecond of current date
+        totalTimeSpent(getCurrentDateMilliSecond(),getUsrDateMilliSecond());
     }
     public void currentDatesAndMilliSecond(){
         Calendar cal = Calendar.getInstance();
@@ -21,18 +24,18 @@ public class MyDate {
         int date  = cal.get(Calendar.DATE);
         cal.clear();
         cal.set(year, month, date);
-        this.currentDate = cal.getTime();
-        this.currentDateMilliSecond = cal.getTimeInMillis();
+        this.currentDate = cal.getTime();//current time
+        this.currentDateMilliSecond = cal.getTimeInMillis();//current time in milliseconds
     }
     public void totalTimeSpent(long currentDateMilliSecond,long usrDateMilliSecond){
         int second = 1000;//1 second has 1000 milliseconds
         int minute = second * 60; // converting minute to milliseconds
         int hour = minute * 60; //converting hour to milliseconds
         int day = hour * 24;//converting day to milliseconds
-        long weeks = day * 7;
-        long month = (long) (weeks * 4.34524);
-        double year = month * 12F;
-        long gap = currentDateMilliSecond - usrDateMilliSecond;
+        long weeks = day * 7;//converting weeks to milliseconds
+        long month = (long) (weeks * 4.34524); //converting months to milliseconds
+        double year = month * 12L; //year to milliseconds
+        long gap = currentDateMilliSecond - usrDateMilliSecond;//the difference
         this.totalDaysSpent = (int) (gap/day);
         this.totalWeeksSpent = (int) (gap/weeks);
         this.totalMonthSpent = (int) (gap/month);
